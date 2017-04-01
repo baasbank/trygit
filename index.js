f80687061d1bbb76e44e6cba1f134003a793e79c
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-01 13:41:33 +0100

    class that consumes the string class

---
+++ index.js
@@ -0,0 +1,5 @@
+const StringClass = require('./string/string-class');
+
+const words = ['Baasbank', 'Raphael', 'Bad', 'Sharp', 'Andela', 'Fellows'];
+const joined = StringClass.concat(words);
+console.log(joined);
