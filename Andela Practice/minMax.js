83c74d88d47322568cd3c124cc9ca03d2a45ffbf
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-02 13:30:31 +0100

    Added check for non-arrays

---
+++ Andela Practice/minMax.js
@@ -0,0 +1,14 @@
+function minMax (arr) {
+    if (Array.isArray(arr)) {
+        var max = Math.max(...arr);
+        var min = Math.min(...arr);
+        return (max, min);
+
+}
+    else {
+        return "Arrays only";
+    }
+}
+var check = minMax ([1, 5, 8, 98, 40000000, 5000000, -2]);
+//console.log(minMax(5));
+console.log(check);
